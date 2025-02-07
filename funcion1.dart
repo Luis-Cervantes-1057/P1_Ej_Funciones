991f68002c4a4cf0c06b3e62250f5bd1ca8913fe
Luis Cesar Cervantes <164534073+Luis-Cervantes-1057@users.noreply.github.com>
2025-02-07 16:47:39 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,18 @@
+// Funcion que no devuelve valor
+// Funcion saludar
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribirNombre(){
+  print('Luis Cesar Cervantes Velazquez');
+}
+
+void main(){
+  // Luis Cesar Cervantes Velazquez 22308051281057
+  // Llamara a la funcion Saludar
+  print('Luis Cesar Cervantes Velazquez 22308051281057');
+  print('Llamando a la funcion saludar');
+  saludar();
+  escribirNombre();
+} 
