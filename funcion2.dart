0b25b33a74fb556bb95ed3584b3e8184b528def9
Luis Cesar Cervantes <164534073+Luis-Cervantes-1057@users.noreply.github.com>
2025-02-10 18:04:23 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,15 @@
+void suma(int a, int b){
+  print('La suma de $a + $b es: ${a + b}');
+}
+
+void resta(int a, int b){
+  print('La resta de $a - $b es: ${a - b}');
+}
+// Funcion main 
+void main(){
+  print("Luis Cesar Cervantes Velazquez 22308051281057");
+  print("Llamando a la funcion suma y resta");
+  suma(10, 5);
+  resta(10, 5);
+}
+
